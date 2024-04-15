b12bccabaf34d1fe3e397fadba684e6c4f2e62e3
Lukas Gübeli <lukas.guebeli@gmx.ch>
2024-04-15 21:27:30 +0200

    weitere Test erstellt unteranderem für activity übersicht

--- src/test/java/ch/uzh/ifi/hase/soprafs24/controller/HabitControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/controller/HabitControllerTest.java
@@ -7,10 +7,12 @@ import ch.uzh.ifi.hase.soprafs24.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.web.server.ResponseStatusException;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -131,6 +133,47 @@ public class HabitControllerTest {
                 .andExpect(jsonPath("$.name").value("Habit1"));
     }
 
+    @Test //GET Mapping "/groups/{groupId}/habits/{habitId}" - CODE 404 Not Found (Error)
+    public void GET_GroupHabitData_InvalidInput_NotFound() throws Exception {
+        String token = "JaZAJ6m4_wh7_ClFK5jr6vvnyRA";
+        String groupId = "group1";
+        String habitId = "habit1";
+        String userId = "user1";
+
+        when(authService.isTokenValid(token)).thenReturn(true);
+        when(authService.getId(token)).thenReturn(userId);
+
+        Group group = new Group();
+        group.setUserIdList(Arrays.asList(userId));
+        when(groupService.getGroupById(groupId)).thenReturn(group);
+
+        Habit habit = new Habit();
+        habit.setName("Habit1");
+        when(habitService.getHabitById(habitId)).thenReturn(Optional.of(habit));
+
+        when(habitStreakService.getStreak(habitId, groupId)).thenReturn(1);
+
+        Map<LocalDate, Map<String, UserStatsStatus>> statusMap = new TreeMap<>();
+        LocalDate date = LocalDate.now();
+        Map<String, UserStatsStatus> userStatusMap = new HashMap<>();
+        userStatusMap.put(userId, UserStatsStatus.SUCCESS);
+        statusMap.put(date, userStatusMap);
+
+        UserStatsEntry userStatsEntry = new UserStatsEntry();
+        userStatsEntry.setUserId(userId);
+        userStatsEntry.setHabitId(habitId);
+        userStatsEntry.setDueDate(date);
+        userStatsEntry.setStatus(UserStatsStatus.SUCCESS);
+
+        when(userStatsEntryService.getEntriesByUserIdAndHabitId(userId, habitId)).thenThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "No group with id " + groupId + " found."));
+
+
+        mockMvc.perform(get("/groups/{groupId}/habits/{habitId}", groupId, habitId)
+                        .header("Authorization", token)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
     /**
      * ------------------------------ END GET TESTS ------------------------------ START POST TESTS ------------------------------
      */
@@ -174,6 +217,28 @@ public class HabitControllerTest {
                 .andExpect(status().isCreated());
     }
 
+    @Test //POST Mapping "/groups/{groupId}/habits" - CODE 409 Unauthorized (Error)
+    public void POST_Create_GroupHabits_InvalidInput() throws Exception {
+        String token = "JaZAJ6m4_wh7_ClFK5jr6vvnyRA";
+        String groupId = "group1";
+        String userId = "user1";
+        String habitData = "{\"name\":\"Habit1\",\"description\":\"Description1\",\"rewardPoints\":10,\"repeatStrategy\":{\"type\":\"DAILY\"}}";
+
+        when(authService.isTokenValid(token)).thenReturn(true);
+        when(authService.getId(token)).thenReturn(userId);
+
+        Group group = new Group();
+        group.setUserIdList(Arrays.asList(userId));
+        when(groupService.getGroupById(groupId)).thenReturn(group);
+
+        when(groupService.isUserAdmin(userId, groupId)).thenReturn(false);
+
+        mockMvc.perform(post("/groups/{groupId}/habits", groupId)
+                        .header("Authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(habitData))
+                .andExpect(status().isUnauthorized());
+    }
 
     /**
      * ------------------------------ END POST TESTS ------------------------------ START PUT TESTS ------------------------------
