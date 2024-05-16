812dbb7946506461ed4cf6d2aeeb8a2528a0fddf
Simon Hafner <simon.hafner3@uzh.ch>
2024-05-16 11:21:45 +0200

    Update WebSocketConfig.java

--- src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
@@ -28,8 +28,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOrigins("*")
-                .setHandshakeHandler(new DefaultHandshakeHandler())
-                .addInterceptors(new AuthHandshakeInterceptor(authService));
+                .withSockJS();
     }
+
 
 }
